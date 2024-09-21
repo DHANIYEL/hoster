fc133bc22c0cbf25148dfe38dfdda8d8d9f09f8d
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-09-21 12:35:08 +0530

    hamburger done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,58 @@
+import React, { useState } from "react";
 import phoneImage from "../assets/phone-img.png";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-  // const nav = () => {
-  //   const [toggle, setToggle] = useState(false);
-  //   const handleToggle = () => {
-  //     setToggle(!toggle);
-  //   };
-  // };
+  const NavLinks = ({ handleToggle }: { handleToggle: () => void }) => {
+    return (
+      <>
+        <li className="flex items-center max-md:justify-center ">
+          <a href="/">Home</a>
+        </li>
+        <div className="flex gap-7 max-md:flex-col max-md:py-7 items-center">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </div>
+        <li className="flex items-center justify-end gap-1 max-md:justify-center">
+          <p className="mr-4 text-gray-300 max-md:hidden">|</p>
+          <img src={phoneImage} alt="logo" className="w-4 h-4" />
+          <a href="tel:+919895987044">+91 9656210077</a>
+        </li>
+      </>
+    );
+  };
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div>
-      <div className="flex justify-center py-7 px-40 max-lg:px-20 items-center ">
-        <ul className="flex justify-around w-full font-medium max-md:hidden mx-auto">
-          <li className="flex items-center">
-            <a href="/">Home</a>
-          </li>
-          <div className="flex gap-7">
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#pricing">Pricing</a>
-            </li>
-            <li>
-              <a href="#blog">Blog</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </div>
-          <li className="flex items-center justify-end gap-1">
-            <p className="mr-4 text-gray-300">|</p>
-            <img src={phoneImage} alt="logo" className="w-4 h-4" />
-            <a href="tel:+919895987044">+91 9656210077</a>
-          </li>
+      <div className="flex justify-center py-7 flex-wrap max-lg:px-10 lg:px-20 items-center max-md:justify-end ">
+        <ul className="flex justify-around w-full font-medium flex-wrap text-centers max-md:hidden mx-auto">
+          <NavLinks handleToggle={handleToggle} />
         </ul>
+        <div className="md:hidden">
+          <button onClick={handleToggle}>{toggle ? <X /> : <Menu />}</button>
+        </div>
       </div>
+      {toggle && (
+        <div className="md:hidden list-none flex items-center basis-full justify-center flex-col ">
+          <NavLinks handleToggle={handleToggle} />
+        </div>
+      )}
     </div>
   );
 };
